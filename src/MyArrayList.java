039b4a0f3bb39f79df622f7295cf638c9172c4e6
azawhat <azamat.erezhepbayev@gmail.com>
2023-04-18 21:01:33 +0600

    Defining variables and adding MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,18 @@
-public class MyArrayList {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyArrayList<E> {
+
+    private static final int INITIAL_CAPACITY = 10;
+
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        this.elements = new Object[INITIAL_CAPACITY];
+        this.size = 0;
+    }
 
 }
