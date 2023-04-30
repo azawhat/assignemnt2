1fc338d4e208e2d3dee5356b3f6374b2b3d8cd24
azawhat <azamat.erezhepbayev@gmail.com>
2023-04-30 16:37:44 +0600

    Creating constructor to the MyArrayListStacky

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -3,6 +3,11 @@ import java.util.EmptyStackException;
 public class MyArrayListStack<T> {
     private MyArrayList<T> stack;
 
+    public MyArrayListStack(){
+        stack = new MyArrayList<T>();
+    }
+
+
 
 
 
