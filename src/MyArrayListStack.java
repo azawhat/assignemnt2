400efec576a0cc130724b9d8dbedb24f79708901
azawhat <azamat.erezhepbayev@gmail.com>
2023-04-30 16:41:34 +0600

    Adding method size

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -25,9 +25,12 @@ public class MyArrayListStack<T> {
         return stack.get(stack.size() - 1);
     }
 
-
     public boolean isEmpty(){
         return stack.isEmpty();
+    }
+
+    public int size(){
+        return stack.size();
     }
 
 
