030a173f48a8d8be5e08ab8a7c060e8da04b924a
azawhat <azamat.erezhepbayev@gmail.com>
2023-04-30 16:25:22 +0600

    Implementing peek method for Linked list stack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,11 +1,30 @@
-java.util.EmptyStackException;
+import java.util.EmptyStackException;
+
+
 public class MyLinkedListStack<T> {
     private MyLinkedList<T> stack;
     public MyLinkedListStack() {
         stack = new MyLinkedList<>();
     }
+
     public void push(T element) {
-        stack.addFirst(element);
+        stack.add(element);
     }
+
+    public T pop(){
+        if (stack.isEmpty())  {
+            throw new EmptyStackException();
+        }
+        return stack.remove(0);
+    }
+
+    public T peek(){
+        if (stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.get(0);
+    }
+
+
 
 }
