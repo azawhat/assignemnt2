78c4bb66703553e0ee1b2ea5092978cc432f561d
azawhat <azamat.erezhepbayev@gmail.com>
2023-04-30 18:47:31 +0600

    Creating class MyArrayListQueue in ArrayListQueue class

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -3,6 +3,9 @@ import java.util.NoSuchElementException;
 public class MyArrayListQueue<E> {
     private MyArrayList<E> queue;
 
+    public MyArrayListQueue(){
+        queue = new MyArrayList<E>();
+    }
 
 
 
